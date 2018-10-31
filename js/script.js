489d81598ed67a1a95ce328757ffef0117ec4530
Jose Camacho <josecamacho@students.ptechnyc.org>
2018-10-31 21:45:54 +0000

    I was able to make a click handler for user and the computer picks a random move

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+$("#shoot").click(function(){
+    $("#userChoice").text($("#input").val());
+});
+
+
+
+var rps = ["rock", "paper", "scissors"];
+
+
+var randomChoice =
+rps [Math.floor(Math.random()* rps.length)];
+
+$("#computerChoice").append(randomChoice);
+
+    
+
