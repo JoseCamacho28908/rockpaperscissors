f8b28b06fde2a8ee7349442c1e84fbf17df74461
Jose Camacho <josecamacho@students.ptechnyc.org>
2018-11-18 22:56:50 +0000

    I was able to make the rock, paper, scissors game

--- js/script.js
+++ js/script.js
@@ -18,10 +18,47 @@ $("#shoot").click(function(){
 var rps = ["rock", "paper", "scissors"];
 
 
-var randomChoice =
-rps [Math.floor(Math.random()* rps.length)];
 
-$("#computerChoice").append(randomChoice);
 
+
+
+$("#shoot").click(function(){
+    
+var randomChoice = rps [Math.floor(Math.random() * rps.length)];
+ $("#computerChoice").html(randomChoice);
+ $("#computerChoice").hide(randomChoice);
+ 
+  var option = $("#input").val();
+  $("#computerChoice").show(randomChoice);
+  
+  if (option === "rock" && randomChoice === "scissors" ) {
+    $("#winner").html("You wins! :)");
+} 
+  else if (option === "scissors" && randomChoice === "paper"){
+    $("#winner").html("You win! :)");
+}
+
+  else if (option === "paper" && randomChoice === "rock"){
+    $("#winner").html("You win! :)");
+}
+  else if (option === "paper" && randomChoice === "paper"){
+    $("#winner").html("It's a draw");
+}
+  else if (option === "rock" && randomChoice === "rock"){
+    $("#winner").html("It's a draw");
+}
+  else if (option === "scizzors" && randomChoice === "scizzors"){
+    $("#winner").html("It's a draw");
+}
+  else{
+    $("#winner").html("You lose! :(");  
+  }
+});
+ 
+
+    
+
+    
+    
     
 
