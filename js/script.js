cc639d6cb8936ada690ad1bfcaad641e413ce1dd
Jose Camacho <josecamacho@students.ptechnyc.org>
2018-12-03 17:19:38 +0000

    Organized code

--- js/script.js
+++ js/script.js
@@ -9,65 +9,65 @@
 
 // DOCUMENT READY FUNCTION BELOW
 var UserScore = 0;
-var ComputerScore=0;
+var ComputerScore = 0;
 
 $("#userScore").html(UserScore);
 $("#computerScore").html(ComputerScore);
 
-$("#shoot").click(function(){
-    $("#userChoice").text($("#input").val());
+$("#shoot").click(function() {
+  $("#userChoice").text($("#input").val());
 });
 
 var rps = ["Rock", "Paper", "Scissors"];
 
-$("#shoot").click(function(){
-    
-var randomChoice = rps [Math.floor(Math.random() * rps.length)];
- $("#computerChoice").html(randomChoice);
- $("#computerChoice").hide(randomChoice);
- 
+$("#shoot").click(function() {
+
+  var randomChoice = rps[Math.floor(Math.random() * rps.length)];
+  $("#computerChoice").html(randomChoice);
+  $("#computerChoice").hide(randomChoice);
+
   var option = $("#input").val();
   $("#computerChoice").show(randomChoice);
-  
-while (option !== "Rock" && option!=="Paper" && option!== "Scissors"){
-  alert("Please type Rock, Paper, or Scissors");
-  return false;
- 
-}  
-  
-  if (option === "Rock" && randomChoice === "Scissors" ) {
-    $("#winner").html("You win! :) ");
-} 
-  else if (option === "Scissors" && randomChoice === "Paper"){
-    $("#winner").html("You win! :) ");
-}
 
-  else if (option === "Paper" && randomChoice === "Rock"){
-    $("#winner").html("You win! :) ");
-}
-  else if (option === "Paper" && randomChoice === "Paper"){
-    $("#winner").html("It's a draw");
-}
-  else if (option === "Rock" && randomChoice === "Rock"){
-    $("#winner").html("It's a draw");
-}
-  else if (option === "Scizzors" && randomChoice === "Scizzors"){
-    $("#winner").html("It's a draw");
-}
-  else{
-    $("#winner").html("You lose! :( ");  
-}
+  while (option !== "Rock" && option !== "Paper" && option !== "Scissors") {
+    alert("Please type Rock, Paper, or Scissors");
+    return false;
 
-if ($("#winner").html() === "You win! :)"){
-  UserScore+=1;
-}
-else if ($("#winner").html() === "It's a draw"){
-  UserScore+=1; 
-  ComputerScore+=1;
-}
-else {
-ComputerScore+=1;
-}
-$("#userScore").html(UserScore);
-$("#computerScore").html(ComputerScore);
+  }
+
+  if (option === "Rock" && randomChoice === "Scissors") {
+    $("#winner").html("You win! :) ");
+  }
+  else if (option === "Scissors" && randomChoice === "Paper") {
+    $("#winner").html("You win! :) ");
+  }
+
+  else if (option === "Paper" && randomChoice === "Rock") {
+    $("#winner").html("You win! :) ");
+  }
+  else if (option === "Paper" && randomChoice === "Paper") {
+    $("#winner").html("It's a draw");
+  }
+  else if (option === "Rock" && randomChoice === "Rock") {
+    $("#winner").html("It's a draw");
+  }
+  else if (option === "Scizzors" && randomChoice === "Scizzors") {
+    $("#winner").html("It's a draw");
+  }
+  else {
+    $("#winner").html("You lose! :( ");
+  }
+
+  if ($("#winner").html() === "You win! :) ") {
+    UserScore += 1;
+  }
+  else if ($("#winner").html() === "It's a draw") {
+    UserScore += 1;
+    ComputerScore += 1;
+  }
+  else {
+    ComputerScore += 1;
+  }
+  $("#userScore").html(UserScore);
+  $("#computerScore").html(ComputerScore);
 });
